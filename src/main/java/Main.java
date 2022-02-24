a406dceac41d1076b53f56df06164502cd107298
KrYounger <35814586+xt0fer@users.noreply.github.com>
2022-02-24 13:58:30 -0500

    try again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,12 @@
 public class Main {
 
     public static void main(String[] args){
+        Main main_object = new Main();
 
+        System.out.println(main_object.sumOfNumbers(5));
+    }
+
+    int sumOfNumbers(int n) {
+        return 0;
     }
 }
